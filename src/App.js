b8fc6831ca4a0e44124c0c096740b747d8a84320
karthiksanthamoorthy <karthiksanthamoorthy713@gmail.com>
2023-02-09 04:59:24 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Gmail from './component/Gmail';
-//import Login from './component/login';
 
 function App() {
   
   return (
     <div className="App">
-       <Gmail /> 
-       {/* <Login /> */}
+       <Gmail />        
     </div>
   );
 }
